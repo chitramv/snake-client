f4127b35ebc5a98a193e687b537c61bdc5c68544
Chitra <chitravenkitraman@gmail.com>
2021-09-22 15:19:48 -0600

    added user interface to terminate the process

--- play.js
+++ play.js
@@ -26,3 +26,25 @@ conn.on('connect', () => {
       },2000)
 
 });
+// setup interface to handle user input from stdin
+const stdin = process.stdin;
+// don't worry about these next two lines of setup work.
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+if ( key === '\u0003') {
+  process.exit();// if ctrl + c is pressed then connection should terminate
+}
+};
+stdin.on("data", handleUserInput);
+
+setupInput();
