fd3ce9636df3048de701f86cc500c770e22072ac
Chitra <chitravenkitraman@gmail.com>
2021-09-22 13:05:30 -0600

    added name to snake

--- play.js
+++ play.js
@@ -5,5 +5,8 @@ const {connect} = require('./client');
 console.log("Connecting ...");
 const conn = connect();
 conn.on("connect", () => {
-  console.log("connected");// code that does something when the connection is first established
+  console.log("successfully connection established");// code that does something when the connection is first established
+});
+conn.on('connect', () => {
+  conn.write('Name: CMV');
 });
